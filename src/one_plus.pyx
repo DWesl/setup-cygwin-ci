765bcbb03b6cb5f4dadf15a37b70485502564259
DWesl <22566757+DWesl@users.noreply.github.com>
2021-04-06 19:43:59 -0400

    Add a Cython extension to see if dlopen causes problems.
    I hope this is where problems crop up.  If it isn't, I'm going to have
    to do a lot more work to pin down the problem.

---
+++ src/one_plus.pyx
@@ -0,0 +1,14 @@
+"""Test module with a simple function."""
+
+def one_plus(int x):
+    """Return x + 1.
+
+    Parameters
+    ----------
+    x: int
+
+    Returns
+    -------
+    int
+    """
+    return x + 1
